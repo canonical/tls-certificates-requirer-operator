ce9e4273108e802d6e26fc923df040cd2a052b60
guillaume <guillaume.belanger27@gmail.com>
2023-05-15 11:17:15 -0400

    Doesn't validate configuration values on relation joined

--- src/charm.py
+++ src/charm.py
@@ -16,7 +16,14 @@ from charms.tls_certificates_interface.v2.tls_certificates import (  # type: ign
     generate_csr,
     generate_private_key,
 )
-from ops.charm import ActionEvent, CharmBase, EventBase, InstallEvent
+from ops.charm import (
+    ActionEvent,
+    CharmBase,
+    ConfigChangedEvent,
+    EventBase,
+    InstallEvent,
+    RelationBrokenEvent,
+)
 from ops.main import main
 from ops.model import ActiveStatus, BlockedStatus, SecretNotFoundError, WaitingStatus
 
@@ -35,10 +42,14 @@ class TLSRequirerOperatorCharm(CharmBase):
         """Handles events for certificate management."""
         super().__init__(*args)
         self.certificates = TLSCertificatesRequiresV2(self, "certificates")
-        self.framework.observe(self.on.config_changed, self._request_certificate_hook)
+        self.framework.observe(self.on.config_changed, self._on_config_changed)
         self.framework.observe(self.on.install, self._on_install)
         self.framework.observe(
-            self.on.certificates_relation_joined, self._request_certificate_hook
+            self.on.certificates_relation_joined, self._on_certificates_relation_joined
+        )
+        self.framework.observe(
+            self.on.certificates_relation_broken,
+            self._on_certificates_relation_broken,
         )
         self.framework.observe(self.on.get_certificate_action, self._on_get_certificate_action)
         self.framework.observe(
@@ -98,6 +109,18 @@ class TLSRequirerOperatorCharm(CharmBase):
         )
         logger.info("Private key generated")
 
+    def _on_certificates_relation_broken(self, event: RelationBrokenEvent) -> None:
+        """Removes Certificate from juju secret.
+
+        Args:
+            event: Juju event.
+        """
+        if not self.unit.is_leader():
+            return
+        if self._certificate_is_stored:
+            certificate_secret = self.model.get_secret(label=CERTIFICATE_SECRET_LABEL)
+            certificate_secret.remove_all_revisions()
+
     def _on_certificate_available(self, event: CertificateAvailableEvent) -> None:
         """Stores the certificate in a Juju secret.
 
@@ -111,19 +134,24 @@ class TLSRequirerOperatorCharm(CharmBase):
         if csr_secret_content["csr"].strip() != event.certificate_signing_request:
             logger.info("New certificate CSR doesn't match with the one stored.")
             return
-        self.app.add_secret(
-            content={
-                "certificate": event.certificate,
-                "ca-certificate": event.ca,
-                "chain": json.dumps(event.chain),
-                "csr": event.certificate_signing_request,
-            },
-            label=CERTIFICATE_SECRET_LABEL,
-        )
+        certificate_secret_content = {
+            "certificate": event.certificate,
+            "ca-certificate": event.ca,
+            "chain": json.dumps(event.chain),
+            "csr": event.certificate_signing_request,
+        }
+        if self._certificate_is_stored:
+            certificate_secret = self.model.get_secret(label=CERTIFICATE_SECRET_LABEL)
+            certificate_secret.set_content(content=certificate_secret_content)
+        else:
+            self.app.add_secret(
+                content=certificate_secret_content,
+                label=CERTIFICATE_SECRET_LABEL,
+            )
         logger.info(f"New certificate is stored: {event.certificate}")
         self.unit.status = ActiveStatus()
 
-    def _request_certificate_hook(self, event: EventBase) -> None:
+    def _on_config_changed(self, event: ConfigChangedEvent) -> None:
         """Validates config and requests a new certificate.
 
         Args:
@@ -141,10 +169,36 @@ class TLSRequirerOperatorCharm(CharmBase):
             event.defer()
             return
         if self._certificates_relation_created:
+            self._revoke_existing_certificates()
+            self._generate_csr()
             self._request_certificate()
             self.unit.status = WaitingStatus("Waiting for certificate to be available")
         else:
+            self._generate_csr()
             self.unit.status = ActiveStatus()
+
+    def _on_certificates_relation_joined(self, event: EventBase) -> None:
+        """Validates config and requests a new certificate.
+
+        Args:
+            event: Juju event.
+        """
+        if not self.unit.is_leader():
+            return
+        if not self._csr_is_stored:
+            self.unit.status = WaitingStatus("Waiting for CSR to be generated.")
+            return
+        self._request_certificate()
+        self.unit.status = WaitingStatus("Waiting for certificate to be available")
+
+    def _revoke_existing_certificates(self) -> None:
+        if not self._csr_is_stored:
+            return
+        secret = self.model.get_secret(label=CSR_SECRET_LABEL)
+        secret_content = secret.get_content()
+        self.certificates.request_certificate_revocation(
+            certificate_signing_request=secret_content["csr"].encode()
+        )
 
     def _request_certificate(self) -> None:
         """Requests X.509 certificate.
@@ -152,6 +206,16 @@ class TLSRequirerOperatorCharm(CharmBase):
         Retrieves private key and password from Juju secret, generates a certificate
         signing request (CSR) and inserts it into the `certificates` relation unit relation data.
         """
+        if not self._csr_is_stored:
+            raise RuntimeError("CSR is not stored")
+        csr_secret = self.model.get_secret(label=CSR_SECRET_LABEL)
+        csr_secret_content = csr_secret.get_content()
+        self.certificates.request_certificate_creation(
+            certificate_signing_request=csr_secret_content["csr"].encode()
+        )
+
+    def _generate_csr(self) -> None:
+        """Generates CSR based on private key and stores it in Juju secret."""
         if not self._private_key_is_stored:
             raise RuntimeError("Private key not stored.")
         private_key_secret = self.model.get_secret(label=PRIVATE_KEY_SECRET_LABEL)
@@ -161,8 +225,12 @@ class TLSRequirerOperatorCharm(CharmBase):
             private_key_password=private_key_secret_content["private-key-password"].encode(),
             subject=self._config_subject,
         )
-        self.certificates.request_certificate_creation(certificate_signing_request=csr)
-        self.app.add_secret(content={"csr": csr.decode()}, label=CSR_SECRET_LABEL)
+        csr_secret_content = {"csr": csr.decode()}
+        if self._csr_is_stored:
+            csr_secret = self.model.get_secret(label=CSR_SECRET_LABEL)
+            csr_secret.set_content(content=csr_secret_content)
+        else:
+            self.app.add_secret(content=csr_secret_content, label=CSR_SECRET_LABEL)
 
     @property
     def _private_key_is_stored(self) -> bool:
@@ -171,26 +239,38 @@ class TLSRequirerOperatorCharm(CharmBase):
         Returns:
             bool: Whether private key is stored.
         """
-        try:
-            self.model.get_secret(label=PRIVATE_KEY_SECRET_LABEL)
-            return True
-        except SecretNotFoundError:
-            return False
+        return self._secret_exists(label=PRIVATE_KEY_SECRET_LABEL)
 
     @property
-    def _certificate_is_available(self) -> bool:
+    def _csr_is_stored(self) -> bool:
+        """Returns whether private key is stored.
+
+        Returns:
+            bool: Whether private key is stored.
+        """
+        return self._secret_exists(label=CSR_SECRET_LABEL)
+
+    @property
+    def _certificate_is_stored(self) -> bool:
         """Returns whether certificate is available in Juju secret.
 
         Returns:
             bool: Whether certificate is stored
         """
+        return self._secret_exists(label=CERTIFICATE_SECRET_LABEL)
+
+    def _secret_exists(self, label: str) -> bool:
+        """Returns whether a given secret exists.
+
+        Args:
+            label: Juju secret label
+
+        Returns:
+            bool: Whether the secret exists
+        """
         try:
-            secret = self.model.get_secret(label=CERTIFICATE_SECRET_LABEL)
-            content = secret.get_content()
-            if content.get("certificate", None):
-                return True
-            else:
-                return False
+            self.model.get_secret(label=label)
+            return True
         except SecretNotFoundError:
             return False
 
@@ -202,7 +282,7 @@ class TLSRequirerOperatorCharm(CharmBase):
         """
         if not self.unit.is_leader():
             return
-        if self._certificate_is_available:
+        if self._certificate_is_stored:
             secret = self.model.get_secret(label=CERTIFICATE_SECRET_LABEL)
             content = secret.get_content()
             event.set_results(
--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -5,8 +5,9 @@ import json
 import unittest
 from unittest.mock import Mock, patch
 
+import pytest
 from ops import testing
-from ops.model import WaitingStatus
+from ops.model import ActiveStatus, BlockedStatus, SecretNotFoundError, WaitingStatus
 
 from charm import TLSRequirerOperatorCharm
 
@@ -36,23 +37,33 @@ class TestCharm(unittest.TestCase):
         self.assertEqual(secret["private-key"], private_key)
         self.assertEqual(secret["private-key-password"], private_key_password)
 
-    def test_given_private_key_not_stored_when_on_request_certificate_hook_then_status_is_waiting(
+    def test_given_private_key_not_stored_when_on_config_changed_then_status_is_waiting(
         self,
     ):
         self.harness.set_leader(is_leader=True)
 
-        self.harness.charm._request_certificate_hook(event=Mock())
+        self.harness.update_config(key_values={"subject": "banana.com"})
 
         self.assertEqual(
             self.harness.model.unit.status,
             WaitingStatus("Waiting for private key and password to be generated."),
         )
 
+    def test_given_config_not_valid_when_on_config_changed_then_status_is_blocked(self):
+        self.harness.set_leader(is_leader=True)
+
+        self.harness.update_config(key_values={"subject": ""})
+
+        self.assertEqual(
+            self.harness.model.unit.status,
+            BlockedStatus("Config `subject` must be set."),
+        )
+
     @patch(
         "charms.tls_certificates_interface.v2.tls_certificates.TLSCertificatesRequiresV2.request_certificate_creation"  # noqa: E501, W505
     )
     @patch("charm.generate_csr")
-    def test_given_private_key_is_stored_and_certificate_relation_is_created_when_on_request_certificate_hook_then_certificate_is_requested(  # noqa: E501
+    def test_given_private_key_is_stored_and_certificate_relation_is_created_when_on_config_changed_then_certificate_is_requested(  # noqa: E501
         self,
         patch_generate_csr,
         patch_request_certificate,
@@ -71,9 +82,134 @@ class TestCharm(unittest.TestCase):
         patch_generate_csr.return_value = csr.encode()
         self.harness.add_relation(relation_name="certificates", remote_app="certificates-provider")
 
-        self.harness.charm._request_certificate_hook(event=Mock())
+        self.harness.update_config(key_values={"subject": "banana.com"})
 
         patch_request_certificate.assert_called_with(certificate_signing_request=csr.encode())
+
+    @patch("charm.generate_csr")
+    def test_given_certificate_is_requested_when_on_config_changed_then_status_is_waiting(
+        self,
+        patch_generate_csr,
+    ):
+        csr = "whatever csr"
+        self.harness.set_leader(is_leader=True)
+        self.harness._backend.secret_add(
+            label="private-key",
+            content={
+                "private-key": "whatever private key",
+                "private-key-password": "whatever password",
+            },
+        )
+        patch_generate_csr.return_value = csr.encode()
+        self.harness.add_relation(relation_name="certificates", remote_app="certificates-provider")
+
+        self.harness.update_config(key_values={"subject": "banana.com"})
+
+        self.assertEqual(
+            self.harness.model.unit.status,
+            WaitingStatus("Waiting for certificate to be available"),
+        )
+
+    @patch("charm.generate_csr")
+    def test_given_certificate_relation_is_not_created_when_on_config_changed_then_csr_is_generated(  # noqa: E501
+        self,
+        patch_generate_csr,
+    ):
+        csr = "whatever csr"
+        self.harness.set_leader(is_leader=True)
+        self.harness._backend.secret_add(
+            label="private-key",
+            content={
+                "private-key": "whatever private key",
+                "private-key-password": "whatever password",
+            },
+        )
+        patch_generate_csr.return_value = csr.encode()
+
+        self.harness.update_config(key_values={"subject": "banana.com"})
+
+        secret = self.harness._backend.secret_get(label="csr")
+        assert secret["csr"] == csr
+
+    @patch("charm.generate_csr")
+    def test_given_certificate_relation_is_not_created_when_on_config_changed_then_status_is_active(  # noqa: E501
+        self,
+        patch_generate_csr,
+    ):
+        csr = "whatever csr"
+        self.harness.set_leader(is_leader=True)
+        self.harness._backend.secret_add(
+            label="private-key",
+            content={
+                "private-key": "whatever private key",
+                "private-key-password": "whatever password",
+            },
+        )
+        patch_generate_csr.return_value = csr.encode()
+
+        self.harness.update_config(key_values={"subject": "banana.com"})
+
+        self.assertEqual(
+            self.harness.model.unit.status,
+            ActiveStatus(),
+        )
+
+    @patch(
+        "charms.tls_certificates_interface.v2.tls_certificates.TLSCertificatesRequiresV2.request_certificate_creation"  # noqa: E501, W505
+    )
+    def test_given_csr_is_stored_when_certificates_relation_joined_then_certificate_is_requested(
+        self,
+        patch_request_certificate_creation,
+    ):
+        csr = "whatever csr"
+        self.harness.set_leader(is_leader=True)
+        self.harness._backend.secret_add(label="csr", content={"csr": csr})
+        relation_id = self.harness.add_relation(
+            relation_name="certificates", remote_app="certificates-provider"
+        )
+
+        self.harness.add_relation_unit(
+            relation_id=relation_id, remote_unit_name="certificates-provider/0"
+        )
+
+        patch_request_certificate_creation.assert_called_with(
+            certificate_signing_request=csr.encode()
+        )
+
+    def test_given_csr_is_stored_when_certificates_relation_joined_then_status_is_waiting(
+        self,
+    ):
+        self.harness.set_leader(is_leader=True)
+        self.harness._backend.secret_add(label="csr", content={"csr": "whatever csr"})
+        relation_id = self.harness.add_relation(
+            relation_name="certificates", remote_app="certificates-provider"
+        )
+
+        self.harness.add_relation_unit(
+            relation_id=relation_id, remote_unit_name="certificates-provider/0"
+        )
+
+        self.assertEqual(
+            self.harness.model.unit.status,
+            WaitingStatus("Waiting for certificate to be available"),
+        )
+
+    def test_given_csr_not_stored_when_certificates_relation_joined_then_status_is_waiting(
+        self,
+    ):
+        self.harness.set_leader(is_leader=True)
+        relation_id = self.harness.add_relation(
+            relation_name="certificates", remote_app="certificates-provider"
+        )
+
+        self.harness.add_relation_unit(
+            relation_id=relation_id, remote_unit_name="certificates-provider/0"
+        )
+
+        self.assertEqual(
+            self.harness.model.unit.status,
+            WaitingStatus("Waiting for CSR to be generated."),
+        )
 
     def test_given_csrs_match_when_on_certificate_available_then_certificate_is_stored(self):
         certificate = "whatever certificate"
@@ -82,6 +218,45 @@ class TestCharm(unittest.TestCase):
         csr = "whatever csr"
         self.harness.set_leader(is_leader=True)
         self.harness._backend.secret_add(label="csr", content={"csr": csr})
+
+        self.harness.charm._on_certificate_available(
+            event=Mock(
+                certificate=certificate,
+                ca=ca,
+                chain=chain,
+                certificate_signing_request=csr,
+            )
+        )
+
+        secret = self.harness._backend.secret_get(label="certificate")
+        self.assertEqual(secret["certificate"], certificate)
+        self.assertEqual(secret["ca-certificate"], ca)
+        self.assertEqual(
+            secret["chain"],
+            json.dumps(chain),
+        )
+        self.assertEqual(
+            secret["csr"],
+            csr,
+        )
+
+    def test_given_certificate_already_stored_when_on_certificate_available_then_certificate_is_overwritten(  # noqa: E501
+        self,
+    ):
+        certificate = "whatever certificate"
+        ca = "whatever ca"
+        chain = ["whatever cert 1", "whatever cert 2"]
+        csr = "whatever csr"
+        self.harness.set_leader(is_leader=True)
+        self.harness._backend.secret_add(label="csr", content={"csr": csr})
+        self.harness._backend.secret_add(
+            label="certificate",
+            content={
+                "certificate": "old certificate",
+                "ca-certificate": "old ca certificate",
+                "chain": "old chain",
+            },
+        )
 
         self.harness.charm._on_certificate_available(
             event=Mock(
@@ -134,3 +309,21 @@ class TestCharm(unittest.TestCase):
                 "chain": chain,
             }
         )
+
+    def test_given_certificate_is_stored_when_on_certificates_relation_broken_then_certificate_secret_is_removed(  # noqa: E501
+        self,
+    ):
+        self.harness.set_leader(is_leader=True)
+        self.harness._backend.secret_add(
+            label="certificate",
+            content={
+                "certificate": "whatever",
+                "ca-certificate": "whatever ca",
+                "chain": "whatever chain",
+            },
+        )
+
+        self.harness.charm._on_certificates_relation_broken(event=Mock())
+
+        with pytest.raises(SecretNotFoundError):
+            self.harness._backend.secret_get(label="certificate")
